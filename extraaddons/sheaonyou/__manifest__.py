ad335ed98d9b49213fe6d422bcd77d2cee1fa296
Kenneth02658 <kenneth@senseitechnology.com>
2025-10-21 11:36:28 +0000

    Customization

---
+++ extraaddons/sheaonyou/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Sheaonyou',
+    'version': '1.1',
+    'sequence': 1,
+    'module_type': 'official',
+    'summary': 'Customization for Sheaonyou',
+    'author': 'GeoIworks',
+    'images': [],
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'assets': {
+        'web.assets_frontend': [],
+        'web.assets_backend': [],
+    },
+    'license': 'LGPL-3',
+}
